b29322b147b188cbc49dd0834c5b3e1104f62f82
RD <romualdas@prodev.lt>
2020-12-05 13:29:36 +0200

    Allow Nova login for all users

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,10 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'hello@prodev.lt',
-                'povilas@noriupramogauti.lt',
-            ]);
+            return true;
         });
     }
 
