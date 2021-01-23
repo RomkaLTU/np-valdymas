0975b6d14518db14ad6ae70602fefa50c5a41787
RD <hello@prodev.lt>
2021-01-23 16:49:51 +0200

    Add accessories relationship

--- app/Models/Accessories.php
+++ app/Models/Accessories.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Accessories extends Model
 {
     use HasFactory;
+
+    public function equipment(): BelongsToMany
+    {
+        return $this->belongsToMany(Accessories::class);
+    }
 }
